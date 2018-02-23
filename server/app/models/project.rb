ce810cf70219529daf7a6d40c7915a9666218877
Ubuntu <ubuntu@CSE.lxd>
2018-02-23 18:39:34 +0000

    pushing to test

--- server/app/models/project.rb
+++ server/app/models/project.rb
@@ -1,3 +1,12 @@
 class Project < ApplicationRecord
   belongs_to :division
+  belongs_to :lib
+ 
+  has_many :versions, dependent: :destroy
+
+  #belongs_to :lib
+  #has_many :dependent: :destroy
+
+  validates_presence_of :name, :projectLead, :email
+  
 end
