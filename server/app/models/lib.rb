4bdd45b88ab516e2c17bcf3a2d6bc49c4a7e37f6
Tyler799 <tylerofshellberg@aol.com>
2018-02-25 20:55:34 -0700

    Minor fix in lib model

--- server/app/models/lib.rb
+++ server/app/models/lib.rb
@@ -1,5 +1,5 @@
 class Lib < ApplicationRecord
   belongs_to :os
-  has_one :project, dependent: :destroy
+  has_many :dependencies, dependent: :destroy
   validates_presence_of :name, :verify, :link
 end
