46966a7fe1d80a97772a625cbc88413eac534d16
Owen Parkins <oparkins@gmail.com>
2018-03-09 11:49:43 -0700

    Added Folders to contain components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/components/App.js</code> and save to reload.
         </p>
       </div>
     );
