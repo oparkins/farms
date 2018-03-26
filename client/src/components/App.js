58ec4bccf73366b19d13348322e81c2053981e0a
Tyler799 <tylerofshellberg@aol.com>
2018-03-26 17:10:05 -0600

    Attempt to set up login page, with errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import Login from './Login';
+import Loginscreen from './Loginscreen';
 import '../styles/App.css';
 
 class App extends Component {
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/components/App.js</code> and save to reload.
         </p>
-        <Login/>
+        <Loginscreen/>
       </div>
     );
   }
