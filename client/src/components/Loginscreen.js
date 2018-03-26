bd021a7ea5d0c17844fc92bc7572d1fe74c1abc3
Tyler799 <tylerofshellberg@aol.com>
2018-03-26 17:51:35 -0600

    Removed some .js that I'm preeeeeety sure didn't actually do anything.

--- client/src/components/Loginscreen.js
+++ client/src/components/Loginscreen.js
@@ -4,44 +4,37 @@ import Login from './Login';
 import Button from 'material-ui/Button';
 
 class Loginscreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      loginscreen: [],
-      loginmessage: '',
-      buttonLabel: 'Register',
-      isLogin: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            loginscreen: [],
+            loginmessage: '',
+            buttonLabel: 'Register',
+            isLogin: true
+        }
     }
-  }
-  componentWillMount() {
-    var loginscreen = [];
-    loginscreen.push(<Login parentContext={this} appContext={this.props.parentContext} />);
-    var loginmessage = "";
-    this.setState({
-      loginscreen: loginscreen,
-      loginmessage: loginmessage
-    })
-  }
-  render() {
-    return (
-      <div className="loginscreen">
-        {this.state.loginscreen}
-        <div>
-          {this.state.loginmessage}
-          <MuiThemeProvider>
-            <div>
+    componentWillMount() {
+        var loginscreen = [];
+        loginscreen.push(<Login parentContext={this} appContext={this.props.parentContext} />);
+        var loginmessage = "";
+        this.setState({
+            loginscreen: loginscreen,
+            loginmessage: loginmessage
+        })
+    }
+    render() {
+        return (
+            <div className="loginscreen">
+                {this.state.loginscreen}
             </div>
-          </MuiThemeProvider>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 const style = {
-  margin: 15,
+    margin: 15,
 };
 
 export default Loginscreen;
