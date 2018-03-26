afe3ff09f911495bcbf50699fdb9f3a8d90fa9f2
Tyler799 <tylerofshellberg@aol.com>
2018-03-26 17:46:37 -0600

    No more errors. Technically a log-in page, but very incomplete.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,45 +1,48 @@
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
+import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
+import React, { Component } from 'react';
 
 class Login extends Component {
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:''
-  }
- }
-render() {
-    return (
-      <div>
-        <MuiThemeProvider>
-          <div>
-          <AppBar
-             title="Login"
-           />
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar
+                            title="Login"
+                        />
+                        <TextField
+                            hintText="Enter your Username"
+                            floatingLabelText="Username"
+                            onChange={(event, newValue) => this.setState({ username: newValue })}
+                        />
+                        <br />
+                        <TextField
+                            type="password"
+                            hintText="Enter your Password"
+                            floatingLabelText="Password"
+                            onChange={(event, newValue) => this.setState({ password: newValue })}
+                        />
+                        <br />
+                        <Button variant="raised">
+                            Log In
+                        </Button>
+                    </div>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
 }
 const style = {
- margin: 15,
+    margin: 15,
 };
 export default Login;
--- client/src/components/Loginscreen.js
+++ client/src/components/Loginscreen.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Login from './Login';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
 
 class Loginscreen extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      username:'',
-      password:'',
-      loginscreen:[],
-      loginmessage:'',
-      buttonLabel:'Register',
-      isLogin:true
+    this.state = {
+      username: '',
+      password: '',
+      loginscreen: [],
+      loginmessage: '',
+      buttonLabel: 'Register',
+      isLogin: true
     }
   }
-  componentWillMount(){
-    var loginscreen=[];
-    loginscreen.push(<Login parentContext={this} appContext={this.props.parentContext}/>);
-    var loginmessage = "Not registered yet, Register Now";
+  componentWillMount() {
+    var loginscreen = [];
+    loginscreen.push(<Login parentContext={this} appContext={this.props.parentContext} />);
+    var loginmessage = "";
     this.setState({
-                  loginscreen:loginscreen,
-                  loginmessage:loginmessage
-                    })
+      loginscreen: loginscreen,
+      loginmessage: loginmessage
+    })
   }
   render() {
     return (
@@ -34,10 +32,7 @@ class Loginscreen extends Component {
           {this.state.loginmessage}
           <MuiThemeProvider>
             <div>
-              <Button variant="raised" color="primary">
-                Log In
-              </Button>
-           </div>
+            </div>
           </MuiThemeProvider>
         </div>
       </div>
