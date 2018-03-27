cf38ebb991358a01263caf4ed5ab07b576145930
Tyler799 <tylerofshellberg@aol.com>
2018-03-27 07:53:31 -0600

    Fixed app bar code to match material-UI@next

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,6 +3,8 @@ import AppBar from 'material-ui/AppBar';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import React, { Component } from 'react';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 class Login extends Component {
     constructor(props) {
@@ -17,9 +19,13 @@ class Login extends Component {
             <div>
                 <MuiThemeProvider>
                     <div>
-                        <AppBar
-                            title="Login"
-                        />
+                        <AppBar position="static" color="default">
+                            <Toolbar>
+                                <Typography variant="title" color="inherit">
+                                    F.A.R.M.S: The Free Automated Repository Management System
+                                </Typography>
+                            </Toolbar>
+                        </AppBar>
                         <TextField
                             label="Enter your Username"
                             floatingLabelText="Username"
