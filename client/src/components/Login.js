821014168a48b25a1a05cae8f19be5059f0610a8
Tyler799 <tylerofshellberg@aol.com>
2018-04-09 08:15:29 -0600

    Attempted to add Paper, mostly failed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,6 +5,18 @@ import TextField from 'material-ui/TextField';
 import React, { Component } from 'react';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+
+const styles = theme => ({
+    root: theme.mixins.gutters({
+        paddingTop: 16,
+        paddingBottom: 16,
+        marginTop: theme.spacing.unit * 3,
+    }),
+});
+//const { classes } = props;
 
 class Login extends Component {
     constructor(props) {
@@ -20,29 +32,31 @@ class Login extends Component {
             <div>
                 <MuiThemeProvider>
                     <div>
-                        <TextField
-                            label="Enter your Username"
-                            floatingLabelText="Username"
-                            onChange={(event, newValue) => this.setState({ username: newValue })}
-                        />
-                        <br />
-                        <TextField
-                            type="password"
-                            label="Enter your Password"
-                            floatingLabelText="Password"
-                            onChange={(event, newValue) => this.setState({ password: newValue })}
-                        />
-                        <br />
-                        <Button variant="raised" onClick={(value) => {this.state.changeWindowHandler(1)}}>
-                            Log In
+                        <Paper elevation={4}>
+                            <div>
+                                <TextField
+                                    label="Enter your Username"
+                                    floatingLabelText="Username"
+                                    onChange={(event, newValue) => this.setState({ username: newValue })}
+                                />
+                                <br />
+                                <TextField
+                                    type="password"
+                                    label="Enter your Password"
+                                    floatingLabelText="Password"
+                                    onChange={(event, newValue) => this.setState({ password: newValue })}
+                                />
+                                <br />
+                                <Button variant="raised" onClick={(value) => { this.state.changeWindowHandler(1) }}>
+                                    Log In
                         </Button>
+                            </div>
+                        </Paper>
                     </div>
                 </MuiThemeProvider>
             </div>
         );
     }
 }
-const style = {
-    margin: 15,
-};
-export default Login;
+
+export default withStyles(styles)(Login);
