1001b94171ca68cac4b07052955c2540303945fc
Owen Parkins <oparkins@gmail.com>
2018-04-10 11:22:00 -0600

    Added comments to help understanding.
    Made the delete/add button work. Also completely interfaces with the backend

--- client/src/components/overview/1_companies.js
+++ client/src/components/overview/1_companies.js
@@ -3,6 +3,7 @@ import List, { ListItem, ListItemText } from 'material-ui/List';
 import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 import AddIcon from 'material-ui-icons/Add';
+import DeleteIcon from 'material-ui-icons/Delete';
 import Checkbox from 'material-ui/Checkbox';
 import NetworkManager from '../NetworkManager';
 import Dialog, {DialogTitle} from 'material-ui/Dialog';
@@ -13,30 +14,66 @@ class CompaniesTab extends Component {
         this.state = {
             listItems: [], 
             callback: props.callback,
-            showAddDialog: false
+            showDialog: false, //Shows the dialog box
+            deleteItem: false, //Decide if we are deleting or adding companies
+            checkedItems: [] //Holds the id numbers of the checked items
         }
-        this.getCompanies(this);
+        this.getCompanies(this); //Populate the list with companies
     }
+    
+    /**
+     * Handles the change for the checkbox. It will first figure out if the
+     * item is in the list or not. If it is, then remove the item from the
+     * list. If it is not, add the item to the list. Then, passed on the
+     * size of the list, change the add button to the delete button. This
+     * logic is to allow users to unselect items if they wish.
+     */
+    checkboxHandler = (value) => {
+        console.log("Handling Check Change...");
+        var id = value["id"];
+        var index =this.state.checkedItems.indexOf(id); 
+        
+        //Add or remove?
+        if(index > -1) {
+            this.state.checkedItems.splice(index, 1);
+        } else {
+            this.state.checkedItems.push(id);
+        }
 
-    handleChange = (value) => {
-        //this.state.callback();
-    };
-    handleChange = name => event => {
-        this.setState({ [name]: event.target.checked });
+        //Are we still planning on deleting?
+        if(this.state.checkedItems.length > 0) {
+            this.setState({deleteItem : true});
+        } else {
+            this.setState({deleteItem : false});
+        }
     };
 
-    createListItem = (id, name) => {
+    /**
+     * Creates a template list item. It takes the id and the name
+     * of the company. It will create the list item and return it.
+     * The ID number is used as the value that is passed to the
+     * checkboxHandler method.
+     */
+    createListItem = (id, name, self) => {
+        var _self = self || this;
         console.log("Creating list item...");
-        return (<ListItem button value={id}>
+        return (<ListItem button value={id} key={name + id}>
                     <ListItemText inset primary={name} />
                     <Checkbox
-                        onChange={this.handleChange('checked1')}
-                        value="checked1"
+                        onChange={(event, checked) => {_self.checkboxHandler({id})}}
+                        value='checked1'
                     />
                 </ListItem>);
     }
 
-    addCompany = (value) => {
+
+    /**
+     * Creates and sends the request to add a company to the server.
+     * After the request has been returned, it will update the state
+     * of the component.
+     */
+    addCompany = (_self) => {
+        //TODO: Change the below template to use input values from dialog box
         var tmpData = { 
             name: "Sandia National Laboratories",
             addressLine1: "The",
@@ -48,39 +85,77 @@ class CompaniesTab extends Component {
             phone: "Right",
             email: "contactus@sandia.gov"
         };
-        var thisRef = this;
         NetworkManager.post("/companies", "POST", tmpData).then((data) => {
-            thisRef.setState({showAddDialog: false});
-            thisRef.getCompanies(thisRef);
-            console.log("Success");
+            _self.setState({showDialog: false});
+            _self.getCompanies(_self);
         }).catch((error) => { 
-            //Show some banner? Like a snack bar?
+            //TODO: Show some banner? Like a snack bar? https://material-ui-next.com/demos/snackbars/
             console.log("Error happened with addCompany(); " + error);
         });
 
     }
 
-    getCompanies = (reference) => {
-        if(reference === undefined) {
-            reference = this;
+    /**
+     * Creates the list of companies. It will create the list 
+     * and then update the list once.
+     */
+    getCompanies = (_self) => {
+        if(_self === undefined) {
+            _self = this;
         }
-        
-        console.log("Getting Companies");
         NetworkManager.fetch("/companies", "GET").then((data) => {
             data.json().then(function(data) {
-                var i;
                 var tmp = [];
-                for(i = 0; i < data.length; i++) {
-                    tmp.push(reference.createListItem(data[i]["id"], data[i]["name"]))  
+                for(var i = 0; i < data.length; i++) {
+                    tmp.push(_self.createListItem(data[i]["id"], data[i]["name"], _self));
                 }
-                reference.setState({listItems: tmp});
+                _self.setState({listItems: tmp});
             });   
         }).catch((error) => {
-            //Show an error banner? Make this in app.js?
+            //TODO: Show an error banner? Make this in app.js?
             console.log("Error happened with getCompanies() " + error);
         });
     }
 
+    /**
+     * Creates and sends the request to delete companies. There are two
+     * different requests so that when the last request is sent and returned
+     * it will then update the companies view.
+     */
+    deleteCompanies = (_self) => {
+        _self = _self || this;
+        var i;
+        for(i = 0; i < _self.state.checkedItems.length -1; i++) {
+            NetworkManager.fetch("/companies/" + _self.state.checkedItems[i], "DELETE").then(function(data) {
+                console.log(data);              
+            }).catch(function(error) {
+                console.log(error); //TODO: show error?
+            });
+        }
+        NetworkManager.fetch("/companies/" + _self.state.checkedItems[i], "DELETE").then(function(data) {
+            _self.setState({showDialog: false, deleteItem: false, checkedItems: []});
+            _self.getCompanies(_self);
+        }).catch(function(error) {
+            console.log(error);//TODO: show error?
+        });
+    }
+
+    /**
+     * Handles the floating action button click. Depending
+     * on if we are deleting or adding, it will act accordingly.
+     */
+    buttonHandler = () => {
+        this.setState({showDialog : true});
+        if(this.state.deleteItem) {
+            this.deleteCompanies(this);
+        } else {
+            this.addCompany(this);
+        }
+    }
+
+    /**
+     * Renders the view based on the state of the component.
+     */
     render () {
         const { value } = this.state;
 
@@ -88,28 +163,16 @@ class CompaniesTab extends Component {
             <div>
             <Paper style={{width: '50%', margin: '0 auto'}}>
                 <List component="nav">
-                <ListItem button>
-                    <ListItemText inset primary="Item 1" />
-                    <Checkbox
-                        checked={this.state.checked1}
-                        onChange={this.handleChange('checked1')}
-                        value="checked1"
-                    />
-                </ListItem>
-                <ListItem button>
-                    <ListItemText inset primary="Item 2" />
-                    <Checkbox
-                        checked={this.state.checked2}
-                        onChange={this.handleChange('checked2')}
-                        value="checked2"
-                    />
-                </ListItem>
                 {this.state.listItems}
                 </List>
-                <Button onClick={(value) => {this.setState({showAddDialog : true})}} variant="fab" color='primary' aria-label="add" style={{bottom: 20, right: 20, position: 'fixed'}}>
-                    <AddIcon/>
+                <Button onClick={(value) => {this.setState({showDialog : true})}} variant="fab" color='primary' aria-label="add" style={{bottom: 20, right: 20, position: 'fixed'}}>
+                    { this.state.deleteItem ? <DeleteIcon/> : <AddIcon/>}
                 </Button>
-                <Dialog open={this.state.showAddDialog}><Button onClick={this.addCompany}>Add Test Data</Button> </Dialog>
+                <Dialog open={this.state.showDialog}>
+                    <Button onClick={this.buttonHandler}>
+                        Add Test Data (TODO: Make this a real form for users)
+                    </Button> 
+                </Dialog>
             </Paper>
             </div>
         );
